875aea34b70063ca73a6c6082520ed61f859d6d4
Lucas-O-S <lucasoscomercial@gmail.com>
2025-11-15 20:18:42 -0300

    Feat: Retrieve user from acesstoken

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,20 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { ApiBody, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { ApiBody, ApiResponse, ApiTags, ApiBearerAuth } from "@nestjs/swagger";
 import { LoginDto } from "./dto/login.dto";
 import { LoginSchema } from "./Schema/LoginSchema";
 import { ApiResponseInterface } from "src/App/Interface/ApiResponseInterface";
+import { JwtAuthGuard } from "src/App/guards/JwtAuth.Guard";
 
 
-@Controller("login")
-@ApiTags("Login")
+@Controller("auth")
+@ApiTags("Auth")
 export class AuthController{
     constructor (private readonly service : AuthService ) {}
 
-    @Post()
+    @Post("login")
     @ApiBody(LoginSchema)
-    @ApiResponse({status: 200, description: "usuario atualizado com sucesso"})
+    @ApiResponse({status: 200, description: "login feito com sucesso"})
     @ApiResponse({status: 500, description: "Erro na requisição"})
     async login(@Body() dto : LoginDto) : Promise<ApiResponseInterface>{
         try{
@@ -28,6 +29,29 @@ export class AuthController{
             return{
                 status: 500,
                 message: 'Erro ao fazer login de usuario.',
+                error: error.message || error,
+            }
+        }
+    }
+
+    @Get("retrieve-user")
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @ApiResponse({status: 200, description: "User retrieved successfully"})
+    @ApiResponse({status: 401, description: "Unauthorized"})
+    async retrieveUser(@Request() req) : Promise<ApiResponseInterface>{
+        try{
+            const { id, name } = req.user;
+            return {
+                status: 200,
+                message: 'User retrieved successfully.',
+                dataUnit: { id, name },
+            } ;
+        }
+        catch(error){
+            return{
+                status: 500,
+                message: 'Erro ao recuperar usuario.',
                 error: error.message || error,
             }
         }
